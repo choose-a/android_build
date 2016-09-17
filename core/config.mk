e4490a82a2e25105c9d7b09c63af3dbf4465ca3d
maxwen <max.weninger@gmail.com>
2016-09-17 03:38:43 +0200

    build: fix usage of qcom_target
    Change-Id: I5fbcf7d3b6de2a90a56cd0d695a97df2289830b2

--- core/config.mk
+++ core/config.mk
@@ -640,6 +640,9 @@ else
   DEFAULT_SYSTEM_DEV_CERTIFICATE := build/target/product/security/testkey
 endif
 
+# Rules for QCOM targets
+include $(BUILD_SYSTEM)/qcom_target.mk
+
 # ###############################################################
 # Set up final options.
 # ###############################################################
@@ -879,9 +882,6 @@ endif
 # API Level lists for Renderscript Compat lib.
 RSCOMPAT_32BIT_ONLY_API_LEVELS := 8 9 10 11 12 13 14 15 16 17 18 19 20
 RSCOMPAT_NO_USAGEIO_API_LEVELS := 8 9 10 11 12 13
-
-# Rules for QCOM targets
-include $(BUILD_SYSTEM)/qcom_target.mk
 
 ifneq ($(CUSTOM_BUILD),)
 ## We need to be sure the global selinux policies are included
