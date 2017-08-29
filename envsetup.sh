f9ccab7565f6e5f536152f70dccea8f7ed805099
Humberto Borba <humberos@gmail.com>
2017-08-29 01:31:34 -0300

    build: Update mka for O
    Change-Id: Idd69aba074381a6ded5bda91099fb0f5dbd7e49c
    Signed-off-by: Humberto Borba <humberos@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -1628,7 +1628,7 @@ function mka() {
             make -j `sysctl hw.ncpu|cut -d" " -f2` "$@"
             ;;
         *)
-            schedtool -B -n 1 -e ionice -n 1 make -j `cat /proc/cpuinfo | grep "^processor" | wc -l` "$@"
+            schedtool -B -n 10 -e ionice -n 7 make -j `cat /proc/cpuinfo | grep "^processor" | wc -l` "$@"
             ;;
     esac
 }
