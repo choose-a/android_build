63be58ea0423b836ce7a4881b662fdb64a6f2ef0
Erik Castricum <git@cas-online.nl>
2020-03-08 11:10:25 +0100

    build: unconditionally include vendor boardconfig
    what is the use of conditioning it?
    
    Signed-off-by: Erik Castricum <git@cas-online.nl>
    Change-Id: Ibe969b750caa225eb8eeccc8c1d210aabeae428d

--- core/config.mk
+++ core/config.mk
@@ -233,9 +233,7 @@ include $(BUILD_SYSTEM)/envsetup.mk
 # See envsetup.mk for a description of SCAN_EXCLUDE_DIRS
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
-ifneq ($(CHOOSE_BUILD),)
 include vendor/choose-a/config/BoardConfigChoose.mk
-endif
 
 # The build system exposes several variables for where to find the kernel
 # headers:
